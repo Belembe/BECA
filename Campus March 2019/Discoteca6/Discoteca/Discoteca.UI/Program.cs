45f867debb611deed4cb1d558aa0ba2324933eeb
Paloma Dominguez Sanchez <p.dominguez.sanchez@avanade.com>
2019-03-29 12:52:26 +0100

    Salto linea

--- Campus March 2019/Discoteca6/Discoteca/Discoteca.UI/Program.cs
+++ Campus March 2019/Discoteca6/Discoteca/Discoteca.UI/Program.cs
@@ -15,6 +15,7 @@ namespace Videoteca.UI
         static void Main()
         {
             Application.EnableVisualStyles();
+
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
