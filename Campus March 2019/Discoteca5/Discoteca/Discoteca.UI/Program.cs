428657dcf8f4b5e7078288c8e0d38f41fc8747eb
sergiovaro <sergio-va-ro@hotmail.com>
2019-04-01 08:49:51 +0200

    pruebaSergioV

--- Campus March 2019/Discoteca5/Discoteca/Discoteca.UI/Program.cs
+++ Campus March 2019/Discoteca5/Discoteca/Discoteca.UI/Program.cs
@@ -13,7 +13,7 @@ namespace Videoteca.UI
         /// </summary>
         [STAThread]
         static void Main()
-        {
+        {//Jelous
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
