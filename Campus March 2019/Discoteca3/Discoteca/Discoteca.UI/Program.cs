770cd270e6df34a248b81af513ce1650b727b967
Jose Miguel Castillo Ortiz <josemi.mlg95@gmail.com>
2019-04-01 07:51:39 +0200

    prueba1

--- Campus March 2019/Discoteca3/Discoteca/Discoteca.UI/Program.cs
+++ Campus March 2019/Discoteca3/Discoteca/Discoteca.UI/Program.cs
@@ -15,6 +15,7 @@ namespace Videoteca.UI
         static void Main()
         {
             Application.EnableVisualStyles();
+            //estoy aqui
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
