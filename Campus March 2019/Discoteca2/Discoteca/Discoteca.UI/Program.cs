67543c5e2e3b63851b7da6872ba07e988ec51c45
Jesus Contreras Herrera <jesuscontreras611@gmail.com>
2019-04-01 08:51:34 +0200

    comentario en prueba1 Main

--- Campus March 2019/Discoteca2/Discoteca/Discoteca.UI/Program.cs
+++ Campus March 2019/Discoteca2/Discoteca/Discoteca.UI/Program.cs
@@ -14,6 +14,7 @@ namespace Videoteca.UI
         [STAThread]
         static void Main()
         {
+            //prueba1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
